9310777d4959b3702f42c3d4bc9085272df69ade
Maca <65584068+mrojasb@users.noreply.github.com>
2025-02-25 16:06:11 -0800

    Update index.js with improved code suggestions from copilot

--- pages/detailPage.js
+++ pages/detailPage.js
@@ -12,13 +12,7 @@ export default function detailPage() {
                 <img src="/img/together.jpeg" style={Styles.beStillImage}></img>
 
                 <p style={Styles.body}>
-                    I chose to use watercolor painting because that is the medium I love the most. 
-                    As a woman in tech, finding a mentor was really difficult. Once my mentor came into my life, he was always there for me. 
-                    I have suffered from low self esteem and confidence in the beginning of my career, but my mentor helped me see more clearly what life could be like, and what I was worth. 
-                    After he told me one day that I had potential, and that I was a great coworker, the view I had of myself dramatically shifted.
-                    I started to feel like my doubts were wrong, and that he was right all along, I <strong>was</strong> a great coworker. I do have an awesome personality.
-                    I can never repay the impact that my mentor had on me, because the influence was the catalyst to an evergrowing snowball of mental wellbeing, resilience, and joy that I hold and grow as I continue my journey. 
-                    He only could help me see more clearly - and that has made all the difference. 
+                    I 
                 </p>
 
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,85 +1,68 @@
-
+import { useState } from 'react';
 import afterTap from "../components/afterTap";
 import homeStyle from "../components/homeStyle";
 import Styles from '../components/Theme';
 import Spacer from "../components/Spacer";
 import Link from "next/link";
 
+export default function Index() {
+  const [showIndex2, setShowIndex2] = useState(false);
 
-export default function index() {
+  const handleButtonClick = () => {
+    setShowIndex2(true);
+  };
+
   return (
-    
-  <div>
+    <div>
+      {!showIndex2 && (
         <div id='index1Box' style={homeStyle.startBox}>
-                
           <div style={homeStyle.imgBox}>
-            
-            <div style={homeStyle.imgBox}>
-
-                <img src="/img/room.png" style={homeStyle.roomImg}></img>
-                <div style={homeStyle.arrow}></div>
-                
-                <button 
-                  id='shoulderButton' 
-                  style={homeStyle.shoulderBox}
-                  onClick={function myFunction() {
-                        document.getElementById("index1Box").style.display="none";
-                        document.getElementById("index2Start").style.display="block";
-                  }}           
-                
-                >
-                </button>
-                
-            </div>
-
+            <img src="/img/room.png" style={homeStyle.roomImg} alt="Room" />
+            <div style={homeStyle.arrow}></div>
+            <button 
+              id='shoulderButton' 
+              style={homeStyle.shoulderBox}
+              onClick={handleButtonClick}
+            >
+            </button>
           </div>
-
           <p style={homeStyle.callingTap}>Tap on my shoulder if you need me ☺️</p>
-            
         </div>
+      )}
 
-      {/* --------------------------- After Tap ----------------------------*/}
-
+      {showIndex2 && (
         <div id='index2Start' style={afterTap.index2Start}>
-
-              <div style={afterTap.textBox}>
-                <p style={Styles.body}>
-                              Hey there! 👋
-                              <Spacer height={1.5}/>
-                              My name is Maca. I'm a Researcher, Engineer, 
-                              and more importantly lover of strawberries and blankets
-                              <Spacer height={1}/>
-                              I am still developing this website, but i will be adding more content soon. 
-                              Thank you for stoping by!
-                </p>
-              </div>
-              
-              {/* Nav */}
-              <div style={afterTap.sqNav}>
-
-                  <Link href="/about">
-                    <a style={afterTap.sqNavChild}>About</a>
-                  </Link>
-
-                  <Link href="/resume">
-                    <a style={afterTap.sqNavChild}>Resume</a>
-                  </Link>
-
-                  <Link href="/contact">
-                    <a style={afterTap.sqNavChild}>Contact</a>
-                  </Link>
-              
-              </div>
-
-              {/* image */}
-              <div style={afterTap.tinyMacaBox}>
-                  <img src="/img/tiny-maca.png" style={afterTap.tinyMacaImg}></img>
-              </div>
-
+          <div style={afterTap.textBox}>
+            <p style={Styles.body}>
+              Hey there! 👋
+              <Spacer height={1.5}/>
+              My name is Maca. I'm a Researcher, Engineer, 
+              and more importantly lover of strawberries
+              <Spacer height={1}/>
+              I am still developing this website, but I will be adding more content soon. 
+              Thank you for stopping by!
+            </p>
+          </div>
+          
+          {/* Nav */}
+          <div style={afterTap.sqNav}>
+            <Link href="/about">
+              <a style={afterTap.sqNavChild}>About</a>
+            </Link>
+            <Link href="/resume">
+              <a style={afterTap.sqNavChild}>Resume</a>
+            </Link>
+            <Link href="/contact">
+              <a style={afterTap.sqNavChild}>Contact</a>
+            </Link>
           </div>
 
-  </div>
-
-    
+          {/* image */}
+          <div style={afterTap.tinyMacaBox}>
+            <img src="/img/tiny-maca.png" style={afterTap.tinyMacaImg} alt="Tiny Maca" />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
